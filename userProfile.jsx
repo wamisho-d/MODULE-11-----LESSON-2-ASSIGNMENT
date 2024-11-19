d1072879b68d90a26d132ee67313be6cabe45100
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-18 21:39:51 -0500

    Update userProfile.jsx

---
+++ userProfile.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: 'Alex' };
+        // Binding the changeName method
+        this.changeName = this.changeName.bind(this);
+    }
+
+    // Correct function to update state
+    changeName() {
+        this.setState({ name: 'Charlie' });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>User Profile</h1>
+                <p>Name: {this.state.name}</p>
+                <button onClick={this.changeName}>Change Name</button>
+            </div>
+        );
+    }
+}
+
+export default UserProfile;
