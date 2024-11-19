36db82a6273a769bde642d95fcc1ae7f8437d923
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-11-18 21:39:05 -0500

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MoviesList from './components/MoviesList';
+
+const App = () => {
+    return (
+        <div>
+            <MoviesList />
+        </div>
+    );
+};
+
+export default App;
+
